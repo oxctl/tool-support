a9d07035bfba72d2b78c9fbebbd8a4f94ad111ab
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2020-03-06 12:51:18 +0000

    Don’t try to parse the response, just pass it on.
    We don’t need to actually do anything with the data so it’s pointless having something like Jackson attempt to transform the data. Instead we just pass it on. We shouldn’t be reading this into memory and should really be streaming it between request and response, but it will do for now.

--- src/main/java/uk/ac/ox/ctl/canvasproxy/ProxyController.java
+++ src/main/java/uk/ac/ox/ctl/canvasproxy/ProxyController.java
@@ -3,19 +3,28 @@ package uk.ac.ox.ctl.canvasproxy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RequestCallback;
 import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.ResponseExtractor;
 import org.springframework.web.client.RestTemplate;
 import uk.ac.ox.ctl.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Map;
@@ -38,7 +47,7 @@ public class ProxyController {
 
     @RequestMapping("/api/**")
     @ResponseBody
-    public ResponseEntity<?> proxy(JwtAuthenticationToken principal, RequestEntity<?> requestEntity, @RegisteredOAuth2AuthorizedClient(registrationId = "canvas", required = false) OAuth2AuthorizedClient client) throws URISyntaxException {
+    public ResponseEntity<?> proxy(JwtAuthenticationToken principal, RequestEntity<byte[]> requestEntity, @RegisteredOAuth2AuthorizedClient(registrationId = "canvas", required = false) OAuth2AuthorizedClient client) throws URISyntaxException {
         log.info("It's {}", principal.getName());
         if (client == null) {
             // This is how we signal to the client that we don't have a OAuth token for them.
@@ -47,31 +56,47 @@ public class ProxyController {
             httpHeaders.add("Location", "/tokens/check");
             return new ResponseEntity<>(null, httpHeaders, HttpStatus.FORBIDDEN);
         }
-        String canvasApiBaseUrl = (String)((Map) principal.getTokenAttributes().get("https://purl.imsglobal.org/spec/lti/claim/custom")).get("canvas_api_base_url");
+        String canvasApiBaseUrl = (String) ((Map) principal.getTokenAttributes().get("https://purl.imsglobal.org/spec/lti/claim/custom")).get("canvas_api_base_url");
         URI remoteService = URI.create(canvasApiBaseUrl);
         URI requestUrl = requestEntity.getUrl();
         URI localService = new URI(requestUrl.getScheme(), requestUrl.getUserInfo(), requestUrl.getHost(), requestUrl.getPort(), null, null, null);
         URI thirdPartyApi = new URI(requestUrl.getScheme(), requestUrl.getUserInfo(), remoteService.getHost(), remoteService.getPort(), requestUrl.getPath(), requestUrl.getQuery(), requestUrl.getFragment());
-        HttpHeaders headers = new HttpHeaders();
-        headers.addAll(requestEntity.getHeaders());
-        // If we pass through the wrong host then canvas returns different information.
-        headers.remove("Host");
-        headers.setBearerAuth(client.getAccessToken().getTokenValue());
-        RequestEntity<?> proxyEntity = new RequestEntity<>(requestEntity.getBody(), new HttpHeaders(headers), requestEntity.getMethod(), thirdPartyApi);
         try {
-            ResponseEntity<Object> exchange = restTemplate.exchange(proxyEntity, Object.class);
-            HttpHeaders httpHeaders = new HttpHeaders();
-            httpHeaders.addAll(exchange.getHeaders());
-            exchange.getHeaders().getOrEmpty("Link").stream().map(header -> header.replaceAll(remoteService.toString(), localService.toString())).forEach(header -> httpHeaders.set("Link", header));
-            // We don't want to pass through cookies from Canvas.
-            httpHeaders.remove("Set-Cookie");
-
-            return new ResponseEntity<>(exchange.getBody(), httpHeaders, exchange.getStatusCode());
+            ResponseEntity responseEntity = restTemplate.execute(thirdPartyApi, requestEntity.getMethod(), request -> {
+                HttpHeaders requestHeaders = request.getHeaders();
+                requestHeaders.addAll(requestEntity.getHeaders());
+                // If we pass through the wrong host then canvas returns different information.
+                requestHeaders.remove("Host");
+                requestHeaders.setBearerAuth(client.getAccessToken().getTokenValue());
+                if (requestEntity.getBody() != null) {
+                    request.getBody().write(requestEntity.getBody());
+                }
+            }, response -> {
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.addAll(response.getHeaders());
+                response.getHeaders().getOrEmpty("Link").stream().map(header -> header.replaceAll(remoteService.toString(), localService.toString())).forEach(header -> httpHeaders.set("Link", header));
+                // We don't want to pass through cookies from Canvas.
+                httpHeaders.remove("Set-Cookie");
+                return new ResponseEntity<>(toByteArray(response.getBody()), httpHeaders, response.getStatusCode());
+            });
+            return responseEntity;
         } catch (ResourceAccessException e) {
             log.warn("Failed to load {} exception is: {}", thirdPartyApi, e.getMessage());
             // TODO - Need to fix this so we don't log exception as this will be expected in production
             //  We shouldn't fill the logs with this. We may want a metric on failures.
             throw new RuntimeException(e);
         }
+    }
+
+    byte[] toByteArray(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        int nRead;
+        byte[] data = new byte[1024];
+        while ((nRead = inputStream.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+
+        buffer.flush();
+        return buffer.toByteArray();
     }
 }
