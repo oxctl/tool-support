e9363a38a065fd79d8115601bf2b6adefe32e061
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2020-03-06 08:24:24 +0000

    Use the token requested on behalf of the user.
    Rather than using an admin token we now use a token on behalf of a user which we have obtained using a developer key. This means we can have limited permissions over a user’s account and also means we don’t ever need to have an admin token.

--- src/main/java/uk/ac/ox/ctl/canvasproxy/ProxyController.java
+++ src/main/java/uk/ac/ox/ctl/canvasproxy/ProxyController.java
@@ -4,16 +4,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+import uk.ac.ox.ctl.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Map;
 
 /**
  * This proxy just sends requests on to Canvas. All it does is add the bearer token for the user.
@@ -33,8 +38,17 @@ public class ProxyController {
 
     @RequestMapping("/api/**")
     @ResponseBody
-    public ResponseEntity<?> proxy(RequestEntity<?> requestEntity) throws URISyntaxException {
-        URI remoteService = URI.create("https://oxeval.instructure.com");
+    public ResponseEntity<?> proxy(JwtAuthenticationToken principal, RequestEntity<?> requestEntity, @RegisteredOAuth2AuthorizedClient(registrationId = "canvas", required = false) OAuth2AuthorizedClient client) throws URISyntaxException {
+        log.info("It's {}", principal.getName());
+        if (client == null) {
+            // This is how we signal to the client that we don't have a OAuth token for them.
+            HttpHeaders httpHeaders = new HttpHeaders();
+            // Ideally the client should use this URL to get the user to give consent.
+            httpHeaders.add("Location", "/tokens/check");
+            return new ResponseEntity<>(null, httpHeaders, HttpStatus.FORBIDDEN);
+        }
+        String canvasApiBaseUrl = (String)((Map) principal.getTokenAttributes().get("https://purl.imsglobal.org/spec/lti/claim/custom")).get("canvas_api_base_url");
+        URI remoteService = URI.create(canvasApiBaseUrl);
         URI requestUrl = requestEntity.getUrl();
         URI localService = new URI(requestUrl.getScheme(), requestUrl.getUserInfo(), requestUrl.getHost(), requestUrl.getPort(), null, null, null);
         URI thirdPartyApi = new URI(requestUrl.getScheme(), requestUrl.getUserInfo(), remoteService.getHost(), remoteService.getPort(), requestUrl.getPath(), requestUrl.getQuery(), requestUrl.getFragment());
@@ -42,7 +56,7 @@ public class ProxyController {
         headers.addAll(requestEntity.getHeaders());
         // If we pass through the wrong host then canvas returns different information.
         headers.remove("Host");
-        headers.setBearerAuth(token);
+        headers.setBearerAuth(client.getAccessToken().getTokenValue());
         RequestEntity<?> proxyEntity = new RequestEntity<>(requestEntity.getBody(), new HttpHeaders(headers), requestEntity.getMethod(), thirdPartyApi);
         try {
             ResponseEntity<Object> exchange = restTemplate.exchange(proxyEntity, Object.class);
---
+++ src/main/java/uk/ac/ox/ctl/canvasproxy/WebConfig.java
@@ -0,0 +1,27 @@
+package uk.ac.ox.ctl.canvasproxy;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import uk.ac.ox.ctl.oauth2.client.web.method.annotation.OAuth2AuthorizedClientArgumentResolver;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
+
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    @Override
+    public void addArgumentResolvers(
+            List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(new OAuth2AuthorizedClientArgumentResolver(clientRegistrationRepository, oAuth2AuthorizedClientRepository));
+    }
+}
