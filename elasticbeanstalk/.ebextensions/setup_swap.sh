a79d33d5c845d4ca612a0ae5f01ea48018046755
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2020-03-12 07:51:36 +0000

    Add configuration for elastic beanstalk.
    This needs shippable for it to be deployed and it very similar to the config for the lti-auth-server.

---
+++ elasticbeanstalk/.ebextensions/setup_swap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SWAPFILE=/var/swapfile
+# Create a swapfile the same size as the physical memory
+SWAP_MEGABYTES=$(free -m | awk '$0 ~ /Mem/ {print $2;}')
+
+if [ -f $SWAPFILE ]; then
+  echo "Swapfile $SWAPFILE found, assuming already setup"
+  exit;
+fi
+
+/bin/dd if=/dev/zero of=$SWAPFILE bs=1M count=$SWAP_MEGABYTES
+/bin/chmod 600 $SWAPFILE
+/sbin/mkswap $SWAPFILE
+/sbin/swapon $SWAPFILE
