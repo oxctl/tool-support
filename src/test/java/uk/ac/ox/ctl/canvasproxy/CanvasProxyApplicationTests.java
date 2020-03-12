466b604d24ba85fe2a2ae358118d25ca61bc49c3
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2020-03-12 11:41:48 +0000

    Fix so that we have a ClientRegistrationRepository
    Ideally we should have some classes so that the applications can startup without the configuration, but contain no actual registrations, but as a stopgap measure this allows the tests to pass.

--- src/test/java/uk/ac/ox/ctl/canvasproxy/CanvasProxyApplicationTests.java
+++ src/test/java/uk/ac/ox/ctl/canvasproxy/CanvasProxyApplicationTests.java
@@ -2,8 +2,10 @@ package uk.ac.ox.ctl.canvasproxy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = {"classpath:application.properties", "classpath:application-test.properties"})
 class CanvasProxyApplicationTests {
 
     @Test
