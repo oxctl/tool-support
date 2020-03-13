ae5dc1d54f9fd52f9acefc94e84ee4dae34304fa
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2020-03-13 14:05:14 +0000

    First stab at page to allow for a nice page
    This is a better page for telling people that they have granted access to the tool.

--- src/main/java/uk/ac/ox/ctl/canvasproxy/TokenController.java
+++ src/main/java/uk/ac/ox/ctl/canvasproxy/TokenController.java
@@ -1,19 +1,75 @@
 package uk.ac.ox.ctl.canvasproxy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
-import org.springframework.security.oauth2.core.OAuth2AccessToken;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import uk.ac.ox.ctl.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 
-@RestController()
-@RequestMapping("/token")
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller()
+@RequestMapping("/tokens")
 public class TokenController {
 
-    @GetMapping()
-    public OAuth2AccessToken token(@RegisteredOAuth2AuthorizedClient("canvas") OAuth2AuthorizedClient client) {
-        return client.getAccessToken();
+    private Logger log = LoggerFactory.getLogger(TokenController.class);
+
+    @Value("${canvas.common.css}")
+    private String defaultCommonCss;
+
+    @Value("${canvas.brand.json}")
+    private String defaultBrandJson;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @ModelAttribute("canvasCommonCss")
+    public String canvasCommonCss(JwtAuthenticationToken principal) {
+        String canvasCss = null;
+        if (principal != null && principal.getTokenAttributes().get("https://purl.imsglobal.org/spec/lti/claim/custom") != null) {
+            canvasCss = (String) ((Map) principal.getTokenAttributes().get("https://purl.imsglobal.org/spec/lti/claim/custom")).get("canvas_css_common");
+        }
+        if (canvasCss == null) {
+            canvasCss = defaultCommonCss;
+        }
+        return canvasCss;
+    }
+
+    @ModelAttribute("canvasBrandCss")
+    public String canvasBrandCss(JwtAuthenticationToken principal) {
+        String canvasJson = null;
+        if (principal != null && principal.getTokenAttributes().get("https://purl.imsglobal.org/spec/lti/claim/custom") != null) {
+            canvasJson = (String) ((Map) principal.getTokenAttributes().get("https://purl.imsglobal.org/spec/lti/claim/custom")).get("com_instructure_brand_config_json_url");
+
+            if (canvasJson == null) {
+                canvasJson = defaultBrandJson;
+            }
+        }
+        // This is a fudge as at the moment a CSS version exists alongside the JS version.
+        String canvasCss = null;
+        if (canvasJson != null) {
+            if (canvasJson.endsWith(".json")) {
+                canvasCss = canvasJson.substring(0, canvasJson.length() - ".json".length()) + ".css";
+            }
+        }
+        return canvasCss;
+    }
+
+
+    @RequestMapping("/check")
+    public ModelAndView token(JwtAuthenticationToken authenticationToken, @RegisteredOAuth2AuthorizedClient() OAuth2AuthorizedClient client) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("applicationName", client.getClientRegistration().getClientName());
+        model.put("target", authenticationToken.getToken().getClaim("https://purl.imsglobal.org/spec/lti/claim/target_link_uri"));
+        model.put("error", "None");
+        model.put("message", "None");
+        return new ModelAndView("login-done", model);
     }
 
 }
