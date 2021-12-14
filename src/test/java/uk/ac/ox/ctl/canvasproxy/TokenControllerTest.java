8cdd76571b521e2f3918ed61d493d2ef06fc56cf
christopherjohnson <christophersebastianjohnson@gmail.com>
2021-12-14 09:41:44 +0000

    AB#57334 - Fix CVE-2021-22119 DoS

--- src/test/java/uk/ac/ox/ctl/canvasproxy/TokenControllerTest.java
+++ src/test/java/uk/ac/ox/ctl/canvasproxy/TokenControllerTest.java
@@ -28,7 +28,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
