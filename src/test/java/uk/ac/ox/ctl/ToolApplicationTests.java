f2660c67cdf195f7e93875d81cd5c6c6b56c778b
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2023-03-09 16:41:19 +0000

    We don't need a session for /admin/tools/
    Noticed this when looking at a response, we don't actually need to create sessions for this endpoint and we shouldn't have one as it can prevent us accidentally introducing strange bugs.

--- src/test/java/uk/ac/ox/ctl/ToolApplicationTests.java
+++ src/test/java/uk/ac/ox/ctl/ToolApplicationTests.java
@@ -1,15 +1,27 @@
 package uk.ac.ox.ctl;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(locations = {"classpath:application.properties", "classpath:application-test.properties"})
 class ToolApplicationTests {
-
+    
     @Test
     void contextLoads() {
+    }
+    
+    @Test
+    public void testNoCookies(@Autowired WebTestClient webClient)  {
+        // We shouldn't be setting cookies on this URL as it doesn't need or use a session
+        webClient
+                .get().uri("/admin/tools/")
+                .exchange()
+                .expectStatus().isUnauthorized()
+                .expectCookie().doesNotExist("JSESSIONID");
     }
 
 }
