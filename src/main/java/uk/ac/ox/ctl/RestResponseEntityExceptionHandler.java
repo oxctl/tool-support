596dbc9224f8d0d4b4d18760a32c39c4c6dece91
christopherjohnson <christophersebastianjohnson@gmail.com>
2023-11-23 13:25:54 +0000

    AB#66396 handle errors from misconfigured tools

---
+++ src/main/java/uk/ac/ox/ctl/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,18 @@
+package uk.ac.ox.ctl;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import uk.ac.ox.ctl.canvasproxy.ProxyConfigException;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { ProxyConfigException.class })
+    public ResponseEntity<String> handleException(ProxyConfigException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+}
