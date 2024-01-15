e60f2e515d3d1cb0e860be1036bccb22737ab3c0
christopherjohnson <christophersebastianjohnson@gmail.com>
2024-01-15 13:18:10 +0000

    AB#71555 Remove "sign: false" support.

--- src/test/java/uk/ac/ox/ctl/ltiauth/LtiSettingsTest.java
+++ src/test/java/uk/ac/ox/ctl/ltiauth/LtiSettingsTest.java
@@ -1,17 +1,26 @@
 package uk.ac.ox.ctl.ltiauth;
 
 import org.junit.jupiter.api.Test;
-import uk.ac.ox.ctl.ltiauth.LtiSettings;
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LtiSettingsTest {
-    
+
+    @Test
+    public void testSettings() {
+        LtiSettings settings = new LtiSettings(Duration.ofMinutes(60), "issuer");
+        assertEquals(settings.getExpiration(), Duration.ofMinutes(60));
+        assertEquals(settings.getIssuer(), "issuer");
+    }
+
     @Test
     public void testNoSettings() {
         // Check that when we don't have any settings we don't blow up with a NPE.
-        LtiSettings settings = new LtiSettings(null, false, null, null);
-        assertNull(settings.getClientSettings("test"));
+        LtiSettings settings = new LtiSettings(null, null);
+        assertEquals(settings.getExpiration(), Duration.of(8, ChronoUnit.HOURS));
+        assertEquals(settings.getIssuer(), "https://localhost");
     }
 
 }
