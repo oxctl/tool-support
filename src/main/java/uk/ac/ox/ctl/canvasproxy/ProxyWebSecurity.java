975ede9a1fb3436dd4231ab8d5e8d664ae4d6ec1
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2024-09-06 11:24:07 +0100

    AB#75560 Ensure that we correctly handle sessions.
    We weren't correctly setting up our session which resulted in the session from one user being used to store tokens from a second user.
    
    To reproduce this if you use an application as one user and grant it access to your Canvas account and then use the same application as a different Canvas user it should correctly save the tokens for the different user (and not update the first user).
    
    Also bumped up the Spring Boot version to pull in newer Spring Security versions.

--- src/main/java/uk/ac/ox/ctl/canvasproxy/ProxyWebSecurity.java
+++ src/main/java/uk/ac/ox/ctl/canvasproxy/ProxyWebSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,8 +15,11 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenResolver;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 import uk.ac.ox.ctl.canvasproxy.security.PersistableJwtAuthenticationConverter;
 import uk.ac.ox.ctl.canvasproxy.security.config.annotation.web.configurers.oauth2.client.OAuth2ClientConfigurer;
 import uk.ac.ox.ctl.oauth2.client.userinfo.CanvasUserService;
@@ -86,15 +90,31 @@ public class ProxyWebSecurity {
 
     @Bean
     @Order(13)
-    public SecurityFilterChain checkTokenHttpSecurity(HttpSecurity http) throws Exception {
+    public SecurityFilterChain checkTokenHttpSecurity(HttpSecurity http, SecurityContextRepository contextRepository) throws Exception {
         http.setSharedObject(ClientRegistrationRepository.class, clientRegistrationRepository);
         http.securityMatcher("/tokens/check")
                 .oauth2Client(oauth -> oauth.authorizationCodeGrant(authcode -> authcode.accessTokenResponseClient(accessTokenResponseClient)))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .securityContext(securityContext -> securityContext.securityContextRepository(contextRepository))
                 .cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 // This is the only endpoint that should take the JWT out of the header and use it to start a session.
-                .oauth2ResourceServer(oauth -> oauth.jwt(jwt -> jwt.decoder(jwtDecoder).jwtAuthenticationConverter(new PersistableJwtAuthenticationConverter())).bearerTokenResolver(tokenResolver))
+                .oauth2ResourceServer(oauth -> oauth
+                        .jwt(jwt -> jwt
+                                .decoder(jwtDecoder)
+                                .jwtAuthenticationConverter(new PersistableJwtAuthenticationConverter()))
+                        .bearerTokenResolver(tokenResolver)
+                        // We use this to persist the security context so that when returning from Canvas
+                        // we know who the current user is and can persist the token for them. Normally a bearer token
+                        // isn't persisted across requests but we use the JWT to authenticate the session
+                        .withObjectPostProcessor(new ObjectPostProcessor<>() {
+                            @Override
+                            public Object postProcess(Object object) {
+                                if (object instanceof BearerTokenAuthenticationFilter filter) {
+                                    filter.setSecurityContextRepository(contextRepository);
+                                }
+                                return object;
+                            }}))
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
         ;
         return http.build();
@@ -102,17 +122,26 @@ public class ProxyWebSecurity {
 
     @Bean
     @Order(14)
-    public SecurityFilterChain loginHttpSecurity(HttpSecurity http) throws Exception {
+    public SecurityFilterChain loginHttpSecurity(HttpSecurity http, SecurityContextRepository contextRepository) throws Exception {
         http.setSharedObject(ClientRegistrationRepository.class, clientRegistrationRepository);
         http.securityMatcher("/login/**")
                 .oauth2Client(oauth -> oauth.authorizationCodeGrant(authcode -> authcode.accessTokenResponseClient(accessTokenResponseClient)))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .securityContext(securityContext -> securityContext.securityContextRepository(contextRepository))
                 .cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .oauth2ResourceServer(oauth -> oauth.jwt(jwt -> jwt.decoder(jwtDecoder)).bearerTokenResolver(tokenResolver))
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
         ;
         return http.build();
+    }
+
+    @Bean
+    SecurityContextRepository securityContextRepository() {
+        // We want to save the security context across requests for the OAuth2 flow.
+        // We set a custom context key so we don't accidentally use this security context for anything else
+        HttpSessionSecurityContextRepository httpSessionSecurityContextRepository = new HttpSessionSecurityContextRepository();
+        httpSessionSecurityContextRepository.setSpringSecurityContextKey("OAUTH_TOKEN_GRANT");
+        return httpSessionSecurityContextRepository;
     }
 
 }
