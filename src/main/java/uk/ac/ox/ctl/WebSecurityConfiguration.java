444ca335155e41d7faf5e2db9abb075e75a1cf1c
Matthew Buckett <matthew.buckett@ctl.ox.ac.uk>
2025-08-28 16:36:03 +0100

    AB#83578 Allow testing of Sentry
    This pulls in a small sentry actuator endpoint that allows the configuration to be tested by sending a test event to sentry.

--- src/main/java/uk/ac/ox/ctl/WebSecurityConfiguration.java
+++ src/main/java/uk/ac/ox/ctl/WebSecurityConfiguration.java
@@ -3,12 +3,14 @@ package uk.ac.ox.ctl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -35,6 +37,17 @@ public class WebSecurityConfiguration {
         return new ToolCorsConfigurationSource(toolRepository, origins);
     }
 
+
+    @Bean
+    @Order(Ordered.LOWEST_PRECEDENCE - 1)
+    public SecurityFilterChain actuatorSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http.securityMatcher(EndpointRequest.toAnyEndpoint())
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+
     @Bean
     @Order(Ordered.LOWEST_PRECEDENCE)
     public SecurityFilterChain finalSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -44,7 +57,6 @@ public class WebSecurityConfiguration {
                     "/css/**",
                     "/images/**",
                     "/resources/**",
-                    "/actuator/health",
                     "/error"
                 )
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
